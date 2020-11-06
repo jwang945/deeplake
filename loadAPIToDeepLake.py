e4b02bf0b4d2481cd55c0fe490da940cf6eef21c
Jet Wang <jetwang@Jets-MacBook-Pro-4.local>
2020-11-06 10:49:46 -0500

    added url and response with error catching

--- loadAPIToDeepLake.py
+++ loadAPIToDeepLake.py
@@ -1,1 +1,16 @@
+import requests
+from requests.exceptions import HTTPError
 
+url = "https://cat-fact.herokuapp.com/facts" #edit URL to pick which api to retrieve data from
+
+try:
+    response = requests.get(url)
+
+    # If the response was successful, no Exception will be raised
+    response.raise_for_status()
+except HTTPError as http_err:
+    print(f'HTTP error occurred: {http_err}')
+except Exception as err:
+    print(f'Other error occurred: {err}')
+else:
+    print('Success!')
