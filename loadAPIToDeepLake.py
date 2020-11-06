a7493c7dac1bebeec94966714b055a79569930a3
Jet Wang <jetwang@Jets-MacBook-Pro-4.local>
2020-11-06 11:57:14 -0500

    add file upload def

--- loadAPIToDeepLake.py
+++ loadAPIToDeepLake.py
@@ -1,6 +1,25 @@
 import requests
 from requests.exceptions import HTTPError
 from azure.storage.filedatalake import DataLakeServiceClient
+from azure.storage.filedatalake import DataLakeFileClient
+import datetime
+
+def upload_to_DeepLake(filesystem_name, connection_string, data):
+    #datalake client creation
+    service = DataLakeServiceClient.from_connection_string(conn_str=connection_string)
+    #filesystem_name cannot have capitol letters or /
+    print("Generating file system named: ", filesystem_name)
+
+    #create the filesystem client
+    filesystem_client = service.create_file_system(file_system=filesystem_name)
+
+    #add a file to the filesystem
+    file_name = filesystem_name+"001"
+    file_client = filesystem_client.get_file_client(file_name)
+    file_client.create_file()
+    file_client.append_data(data=data, offset=0, length=len(data))
+    file_client.flush_data(len(data))
+    #filesystem_client.delete_file_system() #clean the filesystem
 
 url = "https://cat-fact.herokuapp.com/facts" #edit URL to pick which api to retrieve data from
 connection_string = 'DefaultEndpointsProtocol=https;AccountName=apitodatalaketest1;AccountKey=EpEARsk4ZxdplPrq6UlfajD7ukz61A+38zWn8Lcq4sB4yXEfyBXZ8H/WhyvSaeAXpP9Dwwmp+gXFoHb53Yq3Mg==;EndpointSuffix=core.windows.net'
@@ -9,14 +28,14 @@ try:
     response = requests.get(url)
     #will only raise a response if unsuccessful
     response.raise_for_status()
-
-    #datalake client creation
-    service = DataLakeServiceClient.from_connection_string(conn_str=connection_string)
-    filesystem_name = "herokuTest1"
-    print("Generating file system named: ", filesystem_name)
+    data = response.text 
+    upload_to_DeepLake("herokutest3", connection_string, data) #filesystem name cannot have capital letters or /
+    
+    
 except HTTPError as http_err:
     print(f'HTTP error occurred: {http_err}')
 except Exception as err:
     print(f'Other error occurred: {err}')
 else:
     print('Success!')
+
